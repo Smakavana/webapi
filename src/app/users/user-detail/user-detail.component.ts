6b608e5fa25c3eb7b0b92bf4c4a898b752b96eb4
Sunanda Makavana <sunanda.makavana@kroger.com>
2018-10-24 16:24:51 -0400

    changes

--- src/app/users/user-detail/user-detail.component.ts
+++ src/app/users/user-detail/user-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../user.service';
+import { User } from '../user.class';
+import { JsonResp } from '../../json-resp.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
+  user : User;
 
-  constructor() { }
+  constructor (private service: UserService, private route: ActivatedRoute) {
+
+   }
 
   ngOnInit() {
+    let id = this.route.snapshot.params.id;
+    this.service.get(id).subscribe(jsonResp => {
+      console.log(jsonResp);
+      this.user = jsonResp.data;
+    })
   }
-
 }
--- src/app/users/user-edit/user-edit.component.ts
+++ src/app/users/user-edit/user-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import {ActivatedRoute } from '@angular/router';
+import { User } from '../user.class';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-user-edit',
@@ -7,9 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
+  user : User;
 
-  ngOnInit() {
+  save() : void {
+    this.service.change(this.user).subscribe(jsonresp => {
+      console.log(jsonresp);
+      if(jsonresp.rc === 200){
+        this.router.navigateByUrl('/users/list');
+      }
+    });
   }
 
+   ngOnInit() {
+    let id = this.route.snapshot.params.id;
+    this.service.get(id).subscribe(jsonresp => {
+      console.log(jsonresp);
+      this.user = jsonresp.data;
+    })
+  }
+
+  constructor(private service: UserService, private route: ActivatedRoute, private router: Router) { }
 }
